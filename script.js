02d9cd37ad1730d49b66d334ea5cdba2aeb20b29
MarisaConway <marisaconway0111@gmail.com>
2019-07-24 13:55:37 -0400

    finished calculator

--- script.js
+++ script.js
@@ -56,16 +56,29 @@ class Calculator {
         this.previousOperand = ''
     }
     getDisplayNumber(number) {
-        const floatNumber = parseFloat(number)
-        if(isNaN(floatNumber)) return ''
-        return number.toLocaleString('en')   
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]  
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
     }
 
 
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
         if (this.operation != null) {
-            this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
         }
     }
 } 
